46236670d25b60cc3864f3aa30dc310f46e7abc0
noaione <noaione0809@gmail.com>
2020-12-17 01:01:20 +0700

    models: create youtube mongoose model

---
+++ src/models/youtube.ts
@@ -0,0 +1,61 @@
+import { createSchema, Type, typedModel, ExtractDoc, ExtractProps } from "ts-mongoose";
+
+const YoutubeVideoSchema = createSchema(
+    {
+        id: Type.string({ required: true }),
+        title: Type.string({required: true}),
+        status: Type.string({required: true}),
+        startTime: Type.number(),
+        endTime: Type.number(),
+        viewers: Type.number(),
+        peakViewers: Type.number(),
+        channel_id: Type.string({required: true}),
+        thumbnail: Type.string({required: true}),
+        group: Type.string({required: true}),
+        platform: Type.string({required: true}),
+    }
+)
+
+const YoutubeChannelSchema = createSchema(
+    {
+        id: Type.string({ required: true }),
+        name: Type.string({required: true}),
+        description: Type.string(),
+        publishedAt: Type.string({required: true}),
+        subscriberCount: Type.number(),
+        viewCount: Type.number(),
+        videoCount: Type.number(),
+        thumbnail: Type.string({required: true}),
+        group: Type.string({required: true}),
+        platform: Type.string({required: true}),
+    }
+)
+
+export type YTVideoProps = ExtractProps<typeof YoutubeVideoSchema>;
+export type YTChannelProps = ExtractProps<typeof YoutubeChannelSchema>;
+
+const YoutubeVideo = typedModel("YoutubeVideo", YoutubeVideoSchema, undefined, undefined, {
+    findByVideo: (yt_id: string) => {
+        // @ts-expect-error
+        return this.find({"id": {"$eq": yt_id}});
+    },
+    findMulVideo: (yt_ids: string[]) => {
+        // @ts-expect-error
+        return this.find({"id": {"$in": yt_ids}});
+    }
+});
+const YoutubeChannel = typedModel("YoutubeChannel", YoutubeChannelSchema, undefined, undefined, {
+    findByChannel: (yt_id: string) => {
+        // @ts-expect-error
+        return this.find({"id": {"$eq": yt_id}});
+    },
+    findMulChannel: (yt_ids: string[]) => {
+        // @ts-expect-error
+        return this.find({"id": {"$in": yt_ids}});
+    }
+});
+
+export {
+    YoutubeVideo,
+    YoutubeChannel
+}
