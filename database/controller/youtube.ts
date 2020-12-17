a7457e503729d293f2f8ae06ea1b20627da5c349
noaione <noaione0809@gmail.com>
2020-12-17 09:18:30 +0700

    database: create database handler

---
+++ database/controller/youtube.ts
@@ -0,0 +1,127 @@
+import axios from "axios";
+import _ from "lodash";
+import { YoutubeChannel, YTChannelProps } from "../../src/models";
+import { logger } from "../../src/utils/logger";
+import { fallbackNaN, filterEmpty } from "../../src/utils/swissknife";
+import { VTuberModel } from "../dataset/model";
+import { version as vt_version } from "../../package.json";
+import { YTRotatingAPIKey } from "../../src/utils/ytkey_rotator";
+
+interface AnyDict {
+    [key: string]: any;
+}
+
+function getBestThumbnail(thumbnails: any, video_id: string): string {
+    if (_.has(thumbnails, "maxres")) {
+        return thumbnails["maxres"];
+    } else if (_.has(thumbnails, "standard")) {
+        return thumbnails["standard"];
+    } else if (_.has(thumbnails, "high")) {
+        return thumbnails["high"];
+    } else if (_.has(thumbnails, "medium")) {
+        return thumbnails["medium"];
+    } else if (_.has(thumbnails, "default")) {
+        return thumbnails["default"];
+    }
+    return `https://i.ytimg.com/vi/${video_id}/maxresdefault.jpg`;
+}
+
+export async function youtubeChannelDataset(dataset: VTuberModel[], apiKeys: YTRotatingAPIKey) {
+    let session = axios.create({
+        headers: {
+            "User-Agent": `vtschedule-ts/${vt_version} (https://github.com/ihateani-me/vtscheduler-ts)`
+        }
+    })
+
+    let parsed_yt_channel: YTChannelProps[] = await YoutubeChannel.find({});
+    let all_parsed_ids = _.map(parsed_yt_channel, "id");
+
+    let toBeParsed = dataset.map((data) => {
+        // @ts-ignore
+        if (all_parsed_ids.includes(data["youtube"])) {
+            return null;
+        }
+        return {"id": data["youtube"], "group": data["id"]};
+    });
+
+    toBeParsed = filterEmpty(toBeParsed);
+
+    const chunked_channels_set = _.chunk(toBeParsed, 40);
+    logger.info(`youtubeChannelDataset() checking channels with total of ${toBeParsed.length} channels (${chunked_channels_set.length} chunks)...`);
+    const items_data_promises = chunked_channels_set.map((chunks, idx) => (
+        session.get("https://www.googleapis.com/youtube/v3/channels", {
+            params: {
+                part: "snippet,statistics",
+                id: _.join(_.map(chunks, "id"), ","),
+                maxResults: 50,
+                key: apiKeys.get()
+            },
+            responseType: "json"
+        })
+        .then((result) => {
+            let yt_result = result.data;
+            let items = yt_result["items"].map((res: any) => {
+                // @ts-ignore
+                let channel_data = _.find(toBeParsed, {"id": res.id});
+                // @ts-ignore
+                res["groupData"] = channel_data["group"];
+                return res;
+            })
+            return items;
+        }).catch((err) => {
+            logger.error(`youtubeChannelDataset() failed to fetch info for chunk ${idx}, error: ${err.toString()}`);
+            return [];
+        })
+    ))
+
+    let items_data: any[] = await Promise.all(items_data_promises).catch((err) => {
+        logger.error(`youtubeChannelDataset() failed to fetch from API, error: ${err.toString()}`)
+        return [];
+    });
+    if (items_data.length < 1) {
+        logger.warn("youtubeChannelDataset() no response from API");
+        return;
+    }
+
+    items_data = _.flattenDeep(items_data);
+    logger.info(`youtubeChannelDataset() preparing update...`);
+    const to_be_committed = items_data.map((res_item) => {
+        let ch_id = res_item["id"];
+        let snippets: AnyDict = res_item["snippet"];
+        let statistics: AnyDict = res_item["statistics"];
+
+        let title = snippets["title"];
+        let desc = snippets["description"];
+        let pubAt = snippets["publishedAt"]
+        let group = res_item["groupData"];
+
+        let thumbs = getBestThumbnail(snippets["thumbnails"], "");
+        let subsCount = 0,
+            viewCount = 0,
+            videoCount = 0;
+
+        if (_.has(statistics, "subscriberCount")) {
+            subsCount = fallbackNaN(parseInt, statistics["subscriberCount"], statistics["subscriberCount"]);
+        }
+
+        // @ts-ignore
+        let finalData: YTChannelProps = {
+            id: ch_id,
+            name: title,
+            description: desc,
+            publishedAt: pubAt,
+            thumbnail: thumbs,
+            subscriberCount: subsCount,
+            viewCount: viewCount,
+            videoCount: videoCount,
+            group: group,
+            platform: "youtube"
+        }
+        return finalData;
+    })
+
+    logger.info(`youtubeChannelDataset() committing update...`);
+    await YoutubeChannel.insertMany(to_be_committed).catch((err) => {
+        logger.error(`youtubeChannelDataset() failed to insert new data, ${err.toString()}`);
+    });
+}
---
+++ database/index.ts
@@ -0,0 +1,131 @@
+
+import { createInterface } from 'readline';
+import mongoose from "mongoose";
+import { YTRotatingAPIKey } from '../src/utils/ytkey_rotator';
+import config from "../src/config.json";
+import { readdirSync, readFileSync } from "fs"
+import { logger } from '../src/utils/logger';
+import { join } from "path";
+import { DatasetModel, VTuberModel } from './dataset/model';
+import _ from 'lodash';
+import { youtubeChannelDataset } from './controller';
+import { TwitchHelix } from '../src/utils/twitchapi';
+import { isNone } from '../src/utils/swissknife';
+
+let mongouri = config.mongodb.uri;
+if (mongouri.endsWith("/")) {
+    mongouri = mongouri.slice(0, -1);
+}
+
+mongoose.connect(`${mongouri}/${config.mongodb.dbname}`, {useNewUrlParser: true, useUnifiedTopology: true});
+
+export function menuController() {
+    console.clear();
+    console.log(
+        "-------------------- Channel Manager --------------------\n" +
+        "Make sure your json dataset in dataset/ folder\n" +
+        "If you want to add custom one, please follow the template\n" +
+        "---------------------------------------------------------\n" +
+        "[1] Initialize new database\n" +
+        "[2] Scrape Missing & Update database list\n" +
+        "[3] Nuke all channel collection\n" +
+        "[4] Nuke database\n" +
+        "[5] Exit\n"
+    );
+    const int = createInterface(process.stdin, process.stdout);
+    int.question("Select: ", async input => {
+        int.close();
+        switch (input) {
+            case "1":
+                await initialize();
+                break;
+            case "2":
+                await propagateAndUpdate();
+                break;
+            case "3":
+                await nukeCollection();
+                break;
+            case "4":
+                await nukeDatabase();
+                break;
+            case "5":
+                process.exit(0);
+            default:
+                return menuController();
+        }
+        delayEnd();
+    })
+}
+
+const delayEnd = () => setTimeout(() => {
+    console.log("Press any key to continue: ");
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
+    process.stdin.on('data', process.exit.bind(process, 0));
+}, 600);
+
+async function scrapeAndUpdate(filename: string, twitchAPI?: TwitchHelix) {
+    let datasetRead: DatasetModel = JSON.parse(readFileSync(join(__dirname, "dataset", filename), "utf-8"));
+    let youtubeData: VTuberModel[] = datasetRead.vliver.filter(f => _.has(f, "youtube")).map((d) => {
+        d["id"] = datasetRead["id"];
+        return d;
+    });
+    let bilibiliData: VTuberModel[] = datasetRead.vliver.filter(f => _.has(f, "bilibili")).map((d) => {
+        d["id"] = datasetRead["id"];
+        return d;
+    });
+    let ttvData: VTuberModel[] = datasetRead.vliver.filter(f => _.has(f, "twitch")).map((d) => {
+        d["id"] = datasetRead["id"];
+        return d;
+    });
+    let twcastData: VTuberModel[] = datasetRead.vliver.filter(f => _.has(f, "twitcasting")).map((d) => {
+        d["id"] = datasetRead["id"];
+        return d;
+    });
+    if (config.workers.youtube) {
+        // 25 secs
+        let ytRotator = new YTRotatingAPIKey(config.youtube.api_keys, 0.4166666675);
+        await youtubeChannelDataset(youtubeData, ytRotator);
+    }
+    if (config.workers.bilibili) {
+
+    }
+    if (config.workers.twitcasting) {
+
+    }
+    if (config.workers.twitch && !isNone(twitchAPI)) {
+
+    }
+
+}
+
+async function propagateAndUpdate() {
+    if (config.youtube.api_keys.length < 1 && config.workers.youtube) {
+        logger.error("propagateAndUpdate() missing youtube API keys to use");
+    }
+    let twitchAPI: TwitchHelix;
+    if (config.twitch.client_id && config.twitch.client_secret) {
+        logger.info("propagateAndUpdate() initializing twitch API")
+        twitchAPI = new TwitchHelix(config.twitch.client_id, config.twitch.client_secret);
+        await twitchAPI.authorizeClient();
+    }
+
+    let allDatasets = readdirSync(join(__dirname, "dataset"))
+        .filter(f => f.endsWith(".json"))
+        .flatMap(async (group): Promise<void> => await scrapeAndUpdate(group, twitchAPI));
+    await Promise.all(allDatasets).catch((err) => {
+        logger.error(`propagateAndUpdate() failed to propagate, ${err.toString()}`);
+    });
+}
+
+async function nukeCollection() {
+
+}
+
+async function nukeDatabase() {
+
+}
+
+async function initialize() {
+
+}
