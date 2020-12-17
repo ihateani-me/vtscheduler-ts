fd073d2b7db2b85e392ae8b5fc8cca1ab536e77e
noaione <noaione0809@gmail.com>
2020-12-17 09:17:53 +0700

    dataset: copy vthell dataset to here with some key removed

---
+++ database/dataset/model.ts
@@ -0,0 +1,14 @@
+export interface DatasetModel {
+    id: string
+    name: string
+    vliver: VTuberModel[]
+}
+
+export interface VTuberModel {
+    id: string
+    name: string
+    youtube?: string
+    bilibili?: string
+    twitch?: string
+    twitcasting?: string
+}
