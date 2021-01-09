acd542db0892f6a16447bfc094391b06a9298763
noaione <noaione0809@gmail.com>
2021-01-09 13:01:46 +0700

    crawler: add `;`

--- src/utils/crawler.ts
+++ src/utils/crawler.ts
@@ -1,4 +1,4 @@
-const delayPromise = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+const delayPromise = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 export function resolveDelayCrawlerPromises(requests: Promise<any>[], delayPerRequest: number) {
     const remapRequest = requests.map(async (prom, idx) => {
