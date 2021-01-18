34714bc556f47b9f6f5db6dd07809aa7659536a7
noaione <noaione0809@gmail.com>
2021-01-18 20:38:48 +0700

    models: create Mildom model

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,6 +2,7 @@ export * from "./youtube";
 export * from "./bilibili";
 export * from "./twitcasting";
 export * from "./twitch";
+export * from "./mildom";
 
 export interface FiltersDataConfig {
     channel_ids: string[]
