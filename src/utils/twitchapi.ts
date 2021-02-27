403ebb9ac86ed52a1bd4fb7ac82565e9aaa0f23a
noaione <noaione0809@gmail.com>
2021-02-27 16:14:42 +0700

    twitchapi: handler for twitch GQL

--- src/utils/twitchapi.ts
+++ src/utils/twitchapi.ts
@@ -273,3 +273,75 @@ export class TwitchHelix {
         return main_results;
     }
 }
+
+interface StreamScheduleGQL {
+    id: string
+    isCancelled: boolean
+    cancelledUntil: string | null
+    startAt: string
+    endAt: string
+    title: string
+}
+
+export class TwitchGQL {
+    private session: AxiosInstance;
+    private gqlSchemas: string;
+
+    constructor() {
+        this.session = axios.create({
+            baseURL: "https://gql.twitch.tv",
+            headers: {
+                "Client-Id": "kimne78kx3ncx6brgo4mv6wki5h1ko",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36",
+                "Content-Type": "application/json"
+            }
+        })
+
+        this.gqlSchemas = `query StreamSchedule($login:String,$startDate:Time) {
+            user(login:$login) {
+                channel {
+                    schedule {
+                        id
+                        segments(startingWeekday:"MONDAY",relativeDate:$startDate) {
+                            id
+                            isCancelled
+                            cancelledUntil
+                            startAt
+                            endAt
+                            title
+                        }
+                    }
+                }
+            }
+        }`;
+    }
+
+    async getSchedules(loginName: string, overrideTime?: string): Promise<[StreamScheduleGQL[], any]> {
+        // sample: 2021-02-28T16:59:59.059Z
+        const relativeTime = moment.utc().format("YYYY-MM-DD[T]HH:mm:ss.SSS[Z]");
+        const variables = {
+            "login": loginName,
+            "startDate": isNone(overrideTime) ? relativeTime : overrideTime,
+        }
+
+        let response: AxiosResponse<any>;
+        try {
+            response = await this.session.post("/gql", {
+                query: this.gqlSchemas,
+                variables: variables,
+                operationName: "StreamSchedule",
+            });
+        } catch (err) {
+            logger.error(`twitchGQL.getSchedules() failed to fetch schedule for ${loginName}, ${err.toString()}`);
+            return [[], err];
+        }
+
+        const schedulesNode = _.get(response.data, "data.user.channel.schedule");
+        if (schedulesNode === null) {
+            // No schedules
+            return [[], null];
+        }
+        const scheduleSegments = _.get(schedulesNode, "segments", []);
+        return [scheduleSegments, null];
+    }
+}
